d64f49c507739acf6dd500e94948e3b14db635c2
Joyal <joyal.palackel19@.com>
2022-07-15 18:40:35 +0530

    first commit

--- firebase.js
+++ firebase.js
@@ -1,17 +1,17 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzbjM3D57LlCrP-gyOHBkR_WU4l3oagNY",
-  authDomain: "incredible-52fa3.firebaseapp.com",
-  projectId: "incredible-52fa3",
-  storageBucket: "incredible-52fa3.appspot.com",
-  messagingSenderId: "536401488936",
-  appId: "1:536401488936:web:99650679dc8a73235d191d"
+  apiKey: "###########",
+  authDomain: "###########",
+  projectId: "###########",
+  storageBucket: "###########",
+  messagingSenderId: "###########",
+  appId: "########################",
 };
 
-const app =  !firebase.apps.length 
-    ? firebase.initializeApp(firebaseConfig)
-    : firebase.app();
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
 
 const db = app.firestore();
 
